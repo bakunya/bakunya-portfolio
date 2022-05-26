1662c02861e958be3557bc620cc28e26d4dda98b
bakunya <bakunya@nyaan.kawaii>
2022-05-26 20:14:30 +0700

    add seo in page

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import ThemeProvider from './context/ThemeContext'
 
 import './main.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('bakunya')).render(
   <React.StrictMode>
       <Router>
         <ThemeProvider initialTheme="dark">
